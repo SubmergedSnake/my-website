a78900b11983f670351832e1f99c14bb1a555226
Joonas Heikkinen <solarshadow.jh@gmail.com>
2016-12-30 20:52:57 +0200

    changes

--- src/app/aspirations/aspirations.component.ts
+++ src/app/aspirations/aspirations.component.ts
@@ -11,7 +11,7 @@ import {slideInDownAnimation} from 'app/animations';
       <img src="{{toddler}}" style="width:100%" alt="Pacifier">
       <div class="carousel-caption">
         <h3>Babysteps</h3>
-        <p>As it stands, I'm still learning the very basics of software/web development. Loosing the pacifier is my next goal.</p>
+        <p>As it NOW stands, I'm still learning the very basics of software/web development. Loosing the pacifier is my next goal.</p>
       </div>
     </template>
     <template ngbSlide>
