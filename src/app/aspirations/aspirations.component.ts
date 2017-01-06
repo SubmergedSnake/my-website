c88c94c06bf11e3a01d29bffcb45508b511481a6
Joonas Heikkinen <solarshadow.jh@gmail.com>
2017-01-06 11:26:43 +0200

    Changes to paragraphs, tags

--- src/app/aspirations/aspirations.component.ts
+++ src/app/aspirations/aspirations.component.ts
@@ -4,35 +4,7 @@ import {slideInDownAnimation} from 'app/animations';
 @Component({
   selector: 'app-aspirations',
   animations:[slideInDownAnimation],
-  template:`
-  <div class="btmmarg">
-  <ngb-carousel class="globalwidth aspirewidth">
-    <template ngbSlide>
-      <img src="{{toddler}}" alt="Pacifier">
-      <div class="carousel-caption">
-        <h3>Babysteps</h3>
-        <p>As it NOW stands, I'm still learning the very basics of software/web development. Loosing the pacifier is my next goal.</p>
-      </div>
-    </template>
-    <template ngbSlide>
-      <img src="{{adolescent}}" alt="Young gun">
-      <div class="carousel-caption">
-        <h3>Junior Developer</h3>
-        <p>Disinclined to call myself that just yet, though by some standards I might already qualify for this title.</p>
-      </div>
-    </template>
-      <template ngbSlide>
-      <img src="{{younggun}}" alt="Love and marriage">
-      <div class="carousel-caption">
-        <h3>The ball & chain</h3>
-        <p>The ultimate goal. Turn development/programming into a love affair. Taxing, yet so fulfilling.</p>
-      </div>
-    </template>
-  </ngb-carousel>
-  </div>
-
-      
-  `,
+  templateUrl:'./aspirations.component.html',
   styleUrls: ['./aspirations.component.css']
 })
 export class AspirationsComponent  {
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -7,11 +7,5 @@ import { Component } from '@angular/core';
 })
 export class WelcomeComponent {
 
-    lorem = `This is my personal portfolio. Content-wise, it is very poor. I'm only just
-    taking my first steps in the IT industry, and have very little to show for it. 
-        Nevertheless, as this site is built with Angular 2 (and Bootstrap), it serves as a
-        testament to my dedication to learn new things and improve
-        upon my professional capabilities as a developer. Explore these pages to learn more about me,
-        what I'm interested in etc. If you have suggestions on how to improve this page, or wish to contact me,
-        then...`; 
+ 
 }
